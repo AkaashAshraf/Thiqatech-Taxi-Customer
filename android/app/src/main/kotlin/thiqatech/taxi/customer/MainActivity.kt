e69b97e5bb45314992eb33fee0b4480992621634
Thiqatech <thiqatech@Thiqatechs-MacBook-Pro.local>
2023-08-22 05:42:04 +0400

    first commit

---
+++ android/app/src/main/kotlin/thiqatech/taxi/customer/MainActivity.kt
@@ -0,0 +1,7 @@
+package thiqatech.taxi.customer
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
